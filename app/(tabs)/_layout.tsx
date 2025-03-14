c238c2f835d5d3e2275fb1ea6170c024d72ec834
Akindu Kodithuwakku <akindukodithuwakku@gmail.com>
2025-03-15 00:10:52 +0530

    routings and project setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,107 @@
+// filepath: c:\Users\akind\OneDrive\Desktop\movies\app\(tabs)\_layout.tsx
+import { View, Text, ImageBackground, Image } from 'react-native';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { images } from '@/constants/images';
+import { icons } from '@/constants/icons';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+const Icons = ({focused, title, icon}) => { // reusable custom component
+  
+    if (focused) { // if the tab is focused
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+        return (
+            <ImageBackground
+            source={images.highlight}
+            style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flex: 1,
+                overflow: 'hidden',
+                borderRadius: 9999,
+                minWidth: 112,
+                minHeight: 56,
+            }}
+            >
+            <Image
+                source={icon}
+                style={{ tintColor: '#151312', width: 20, height: 20 }}
+            />
+            <Text className='text-primary text-base font-semibold'>{title}</Text>
+            </ImageBackground>
+        );
+    }
+    return (
+        <Image
+        source={icon}
+        style={{ tintColor: '#151312', width: 20, height: 20 }}
+        />
+  );
+};
 
+const _layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs>
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "profile",
+          headerShown: false,
+            tabBarIcon: ({ focused }) => (
+                <Icons 
+                focused={focused}
+                icon={icons.person}
+                title="Profile"
+                />
+            )
+        }}
+      />
+
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            return <Icons
+                  focused={focused}
+                  icon={icons.save}
+                  title="Saved" />;
+          }
+        }}
+      />
+
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <Icons 
+              focused={focused}
+              icon={icons.search}
+              title="Search"
+              />
+          )
+        }}
+      />
+
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <Icons 
+            focused={focused}
+            icon={icons.home}
+            title="Home"
+            />
+          )
         }}
       />
     </Tabs>
   );
-}
+};
+
+export default _layout;
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#030014',
+        secondary: '#151312',
+
+        light: {
+          100: '#D6C6FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
+        },
+        dark: {
+          100: '#2D2D2D',
+          200: '#1F1F1F',
+          300: '#0C0C0C',
+        },
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  plugins: [
+    require('@tailwindcss/forms'), // Add Tailwind CSS forms plugin
+    require('@tailwindcss/typography'), // Add Tailwind CSS typography plugin
+  ],
+}};
